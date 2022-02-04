3c06ff9dd8b01b1cbbc0b2293b7ac03a326196fb
Raphael Rosa <raphaelrodrigues93@gmail.com>
2022-02-03 21:39:10 -0300

    imprimindo tensao no console.log

--- js/armazenando-dados.js
+++ js/armazenando-dados.js
@@ -3,9 +3,10 @@ const btn = document.querySelector("#send");
 btn.addEventListener('click', function(e){
     e.preventDefault();
 
+   
     const tensao = document.querySelector("#tensao");
-
-    const value = tensao.value
-
+   
+    const value = tensao.value;
     console.log(value);
+
 });
