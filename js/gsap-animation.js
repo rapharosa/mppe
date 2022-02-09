78e1e11e33167d32cf9ed08a327cef5df066cc17
Raphael Rosa <raphaelrodrigues93@gmail.com>
2022-02-09 20:45:25 -0300

    estilização da landing-page

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -1,4 +1,4 @@
-gsap.from('.div, .nav', {opacity: 0, duration: 0.5, delay: 0.7, y:10})
+gsap.from('.div, .nav', {opacity: 0, duration: 0.8, delay: 0.7, y:10})
 
 gsap.from('.titulo-cabecalho, .logo cabecalho, .nav-item', 
-    {opacity: 0, duration: 0.5, delay: 0.7, y: 30, stagger: 0.2})
+    {opacity: 0, duration: 0.8, delay: 0.7, y: 30, stagger: 0.2})
