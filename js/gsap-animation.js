d21ab157275b1a6c98f04685adcf3fd10b5809df
Raphael Rosa <raphaelrodrigues93@gmail.com>
2022-02-09 21:31:13 -0300

    finalização landing-page desktop

--- js/gsap-animation.js
+++ js/gsap-animation.js
@@ -1,4 +1,10 @@
-gsap.from('.div, .nav', {opacity: 0, duration: 0.8, delay: 0.7, y:10})
+gsap.from('.div, .nav', {opacity: 0, duration: 0.8, delay: 0.7, x:10})
 
 gsap.from('.titulo-cabecalho, .logo cabecalho, .nav-item', 
-    {opacity: 0, duration: 0.8, delay: 0.7, y: 30, stagger: 0.2})
+    {opacity: 0, duration: 0.8, delay: 0.7, x: 30, stagger: 0.2})
+
+gsap.from('.titulo, .conteudo-principal-logo, .descricao', 
+    {opacity: 0, duration: 0.8, delay: 0.8, y: 40, stagger: 0.3})
+
+gsap.from('.explicacao, .texto, .papelada', 
+    {opacity: 0, duration: 1, delay: 1, y: 70, stagger: 0.4})
